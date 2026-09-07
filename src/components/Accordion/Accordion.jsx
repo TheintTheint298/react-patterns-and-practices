4a8c62c92547cea1a4d579457ef4609729b96085
TheintTheint298 <theinttheintsoehtet@gmail.com>
2026-09-07 19:18:38 +0630

    feat: grouping compound components

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -1,5 +1,7 @@
 import { createContext, useContext, useState } from "react";
 
+import AccordionItem from "./AccordionItem";
+
 const AccordionContext = createContext();
 
 export function useAccordionContext() {
@@ -17,18 +19,13 @@ export function useAccordionContext() {
 export default function Accordion({ children, className }) {
   const [openItemId, setOpenItemId] = useState();
 
-  function openItem(id) {
-    setOpenItemId(id);
-  }
-
-  function closeItem(id) {
-    setOpenItemId(id);
+  function toggleItem(id) {
+    setOpenItemId((prevId) => (prevId === id ? null : id));
   }
 
   const contextValue = {
     openItemId,
-    openItem,
-    closeItem,
+    toggleItem,
   };
 
   return (
@@ -37,3 +34,5 @@ export default function Accordion({ children, className }) {
     </AccordionContext.Provider>
   );
 }
+
+Accordion.Item = AccordionItem;
